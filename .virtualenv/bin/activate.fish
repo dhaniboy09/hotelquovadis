b2371a31070e6c9c23387267c9613cff9902e7f6
Dhani <Dhani@Dhani-2.local>
2018-07-22 03:33:48 +0100

    re-insall venv

--- .virtualenv/bin/activate.fish
+++ .virtualenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dhani/dev/hotelquovadis/.virtualenv"
+set -gx VIRTUAL_ENV "/Users/dhani/dev/hotelquovadis/quovadisproject/.virtualenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- .virtualenv/bin/django-admin.py
+++ .virtualenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/dhani/dev/hotelquovadis/.virtualenv/bin/python3
+#!/Users/dhani/dev/hotelquovadis/quovadisproject/.virtualenv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
