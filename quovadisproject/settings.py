dd3c6dc689b65676f5f5f3d63932bbe8fe8eca2b
Dhani <Dhani@Dhani-2.local>
2018-07-22 14:04:06 +0100

    Set debug false

--- quovadisproject/settings.py
+++ quovadisproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('QV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hotelquovadisng.com', 'whispering-lake-18060.herokuapp.com']
 
