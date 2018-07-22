b632d841b77916cfee0e0feef68762aa2c3f5d93
Dhani <Dhani@Dhani-2.local>
2018-07-22 05:00:45 +0100

    Handle static files

--- quovadisproject/settings.py
+++ quovadisproject/settings.py
@@ -167,11 +167,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'quovadisapp/static'),
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'quovadisapp/static')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
